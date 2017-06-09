06743c45e7797c5bcf9f81ba3cfe383de021a679
Rzecki <Rzecki@LAPTOP-PT08NKS3>
2017-06-09 17:09:13 +0200

    Changes to database classes.

--- LuggaGo.DataLayer/Models/Airport.cs
+++ LuggaGo.DataLayer/Models/Airport.cs
@@ -9,6 +9,7 @@ namespace LuggaGo.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string ShortName { get; set; }
         public Address Address { get; set; }
 
         public Airport()
---
+++ LuggaGo.DataLayer/Models/LuggagoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LuggaGo.Models
+{
+    public class LuggagoDbContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
